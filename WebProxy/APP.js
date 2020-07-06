03eb1634f7a4cb3b1da54166a0a6a1469b614c07
DAC <dac@cogsmith.com>
2020-07-05 22:35:34 -0400

    1.05

--- WebProxy/APP.js
+++ WebProxy/APP.js
@@ -4,19 +4,34 @@ var toport = process.argv[3] || 8123;
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-var clients = {};
+var App = {};
 
-var LOG = function (msg) { console.log(new Date() + ' :: ' + msg); }
+App.Run = function () {
+    var clients = {};
+    var LOG = function (msg) { console.log(msg); };
 
-var proxy = httpProxy.createProxyServer({target:'http://localhost:'+toport});
-proxy.on('error', function(err) { LOG(err); });
-proxy.on('proxyReq', function (proxyReq,req,res,options) { 
-    var ip=req.connection.remoteAddress; 
-    if (!clients[ip]) { clients[ip]=1; LOG("New IP Seen: "+ip); } else { clients[ip]++; }
-});
+    var proxy = httpProxy.createProxyServer({target:'http://localhost:'+toport});
+    proxy.on('error', function(err) { LOG(err); });
+    proxy.on('proxyReq', function (proxyReq,req,res,options) { 
+        var ip=req.connection.remoteAddress;
+        if (!clients[ip]) { clients[ip]=1; LOG('WebProxy.NewIP:'+ip); } else { clients[ip]++; }
+        //LOG('WebProxy:'+ip+':'+proxyReq.path);
 
-var proxyServer = http.createServer(function (req,res) { proxy.web(req, res, {}); });
-proxyServer.on('upgrade', function (req,socket,head) { proxy.ws(req,socket,head); });
-proxyServer.listen(port);
+        //if (proxyReq.path.startsWith('/local_')) { proxyReq.path='/hassio/ingress'+proxyReq.path; };
+        if (proxyReq.path=='/local_hxjs') { proxyReq.path='/hassio/ingress/local_hxjs'; };
+    });
 
-LOG('Web Proxy Started :: ' + port + ' -> ' + toport);
+    var proxyServer = http.createServer(function (req,res) {
+        var ip=req.connection.remoteAddress;
+        if (ip.substr(0,3)=='10.') { return proxy.web(req,res,{}); }
+        if (ip.substr(0,10)=='::ffff:10.') { return proxy.web(req,res,{}); }
+        res.end('DENY:'+ip);
+    });
+
+    proxyServer.on('upgrade', function (req,socket,head) { proxy.ws(req,socket,head); });
+    proxyServer.listen(port);
+
+    LOG('WebProxy:'+port+'->'+toport);
+};
+
+App.Run();
